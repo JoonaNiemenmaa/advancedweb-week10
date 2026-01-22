1409a09613ddc67b4405eb31e81bf7742752c14e
Joona Niemenmaa <joonaoskari@hotmail.com>
2026-01-22 14:33:29 +0200

    ääh

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,13 +6,16 @@ type TItem = {
 	body: string;
 };
 
+const COUNT = 12;
+
 export default function About() {
 	const [items, setItems] = useState<TItem[]>([]);
+	const [count, setCount] = useState<number>(COUNT);
 
 	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/posts")
 			.then((response) => response.json())
-			.then((json) => {
+			.then((json: []) => {
 				setItems(json);
 			});
 	}, []);
@@ -20,13 +23,25 @@ export default function About() {
 	let key = 0;
 
 	return (
-		<div className="grid-container">
-			{items.map((item) => (
-				<div className="grid-item" key={key++}>
-					<h3>{item.title}</h3>
-					<p>{item.body}</p>
-				</div>
-			))}
-		</div>
+		<>
+			<div className="grid-container">
+				{items.slice(0, count).map((item) => (
+					<div className="grid-item" key={key++}>
+						<h3>{item.title}</h3>
+						<p>{item.body}</p>
+					</div>
+				))}
+			</div>
+			{count <= items.length ? (
+				<button
+					className="more-button"
+					onClick={() => {
+						setCount(count + COUNT);
+					}}
+				>
+					Load more
+				</button>
+			) : null}
+		</>
 	);
 }
